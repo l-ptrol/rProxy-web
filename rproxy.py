1aec7d349668f9452078f78a31e70014640ebb50
Petro1990 <petrolobanok@gmail.com>
2026-03-17 11:22:15 +0300

    v6.5.2: Фикс зависимостей Entware (удаление nginx/certbot из локальной установки)

--- rproxy.py
+++ rproxy.py
@@ -7,7 +7,7 @@ from core.config import ConfigManager
 from core.vps import VPSManager
 from core.manager import ProcessManager
 
-VERSION = "6.5.1"
+VERSION = "6.5.2"
 
 class RProxyCLI:
     def __init__(self):
