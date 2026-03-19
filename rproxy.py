57bdbd24e9310b528880d550018acfd3d77a606e
Petro1990 <petrolobanok@gmail.com>
2026-03-19 14:02:58 +0300

    fix(ui): fix add button, improve mobile layout and suppress fetch errors, bump 7.3.7

--- rproxy.py
+++ rproxy.py
@@ -7,7 +7,7 @@ from core.utils import msg, pause, warn, err, header, draw_separator, get_router
 from core.config import ConfigManager
 from core.vps import VPSManager
 from core.manager import ProcessManager
-VERSION = "7.3.6"
+VERSION = "7.3.7"
 
 class RProxyCLI:
     def __init__(self):
