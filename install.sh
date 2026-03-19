79e621cfbc5413ee821071c5f02fc89353cabc21
Petro1990 <petrolobanok@gmail.com>
2026-03-19 13:08:34 +0300

    feat(edit): added redeploy confirmation after service edit, bump 7.3.5

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # rProxy Web & CLI (Python Core) Installer for Keenetic
 # rProxy Installer v7.2.0
-VERSION="7.3.4"
+VERSION="7.3.5"
 # - Full web interface rebuild
 # Новое ядро на Python. 100% паритет с Bash + Модульность.
 
