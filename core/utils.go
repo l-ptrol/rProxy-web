3ee49157dde5df4b780792511aa04abfc2c4e4a4
Petro1990 <petrolobanok@gmail.com>
2026-03-20 21:34:17 +0300

    v1.1.3-go: Перенос статистики в бейджи вкладок и удаление плиток🧪🛰️🌊🐚🚤🎭🌔

--- core/utils.go
+++ core/utils.go
@@ -23,7 +23,7 @@ const (
 )
 
 // Версия приложения
-const VERSION = "1.1.2-go"
+const VERSION = "1.1.3-go"
 
 // logHookPath — путь к файлу для лог-хука (запись при деплое из веба)
 var logHookPath string
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # rProxy Go Edition
-# Version: 1.1.2-go
-VERSION="1.1.2-go"
+# Version: 1.1.3-go
+VERSION="1.1.3-go"
 
 set -e
 
